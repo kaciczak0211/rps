5509659f8cdb14fe6c6558430c5cb9a9d58ff4f2
kaciczak0211 <kaciczak0211@gmail.com>
2023-04-29 22:58:16 +0100

    js_done

--- main.js
+++ main.js
@@ -1,17 +1,49 @@
-function sample(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
+const player = document.querySelector("#playerText");
+const computer = document.querySelector("#computerText");
+const result = document.querySelector("#result");
+const btn = document.querySelectorAll(".choiceBtn");
 
+let playerChoice;
+let computerChoice;
+let resultGame;
 
-console.log(sample(["rock", "paper", "scissors"]));
+btn.forEach(button => button.addEventListener("click", () => {
+    playerChoice = button.textContent;
+    computerTurn();
+    player.textContent = `Player: ${playerChoice}`;
+    computer.textContent = `Computer: ${computerChoice}`;
+    result.textContent = checkWinner();
 
+}))
 
-function computerChoice(){
+function computerTurn(){
 
-    if(computer === "rock" && sample==="rock"){
-    console.log("Tie!")}
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+      case 1:
+        computerChoice = "Rock";
+        break;
+      case 2:
+        computerChoice = "Paper";
+        break;
+      case 3:
+        computerChoice = "Scissors";
+        break;
+    }
 }
 
-
-const computer = "rock";
-computerChoice();
+function checkWinner(){
+    if(playerChoice == computerChoice){
+        return "Remis";
+    }
+    else if (computerChoice == "Rock"){
+        return(playerChoice == "Paper") ? "You Win!" : "You Lose!"
+    }
+    else if (computerChoice == "Paper"){
+        return(playerChoice == "Scissors") ? "You Lose!" : "You Win!"
+    }
+    else if (computerChoice == "Pcissors"){
+        return(playerChoice == "Rock") ? "You Win!" : "You Lose!"
+    }
+}

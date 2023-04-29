34a3fda36830fc240aae707c1bfb3345293da576
kaciczak0211 <kaciczak0211@gmail.com>
2023-04-29 22:19:05 +0100

    HTML

--- main.js
+++ main.js
@@ -2,4 +2,16 @@ function sample(array) {
     return array[Math.floor(Math.random() * array.length)];
   }
 
+
 console.log(sample(["rock", "paper", "scissors"]));
+
+
+function computerChoice(){
+
+    if(computer === "rock" && sample==="rock"){
+    console.log("Tie!")}
+}
+
+
+const computer = "rock";
+computerChoice();
